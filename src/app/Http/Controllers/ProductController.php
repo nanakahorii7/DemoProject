b5791fdf7f2d9fefa529a14938ec7d525a332790
nhorii <nhorii@gitlab.com>
2023-07-18 00:46:43 +0900

    Createメソッド実装

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -21,9 +21,9 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('products.create');
     }
 
     /**
