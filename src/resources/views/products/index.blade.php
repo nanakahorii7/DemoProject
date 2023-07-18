4fe52e8d4e2f8688611b2d805161008e7796ac39
nhorii <nhorii@gitlab.com>
2023-07-18 11:57:33 +0900

    バグ修正

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Laravel 10 CRUD Example from scratch - ItSolutionStuff.com</h2>
+            <h2>Laravel 10 CRUD Example from scratch</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
@@ -47,6 +47,6 @@
     @endforeach
 </table>
 
-{!! $products->links() !!}
+<!-- {!! $products->links() !!} -->
 
 @endsection
