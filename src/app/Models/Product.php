a081e3fdc05274f2e5cd3c3a77e40f8fb98067a2
nhorii <nhorii@gitlab.com>
2024-02-07 17:54:04 +0900

    修正点

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-{
+{    
     use HasFactory;
+    protected $table = 'products';
     protected $fillable = ['name','detail'];
 }
